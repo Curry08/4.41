3890cf46071bc65d1b490758864e0d2370e03856
anwar <anwarcourri12@gmail.com>
2024-10-24 18:39:07 +0200

    .

---
+++ 4.41.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int lung;
+    printf("Inserisci il numero degli asterischi della base del triangolo: ");
+    scanf("%d",&lung);
+    printf("\n");
+    for(int i=0 ; i<lung ; i++){
+        for (int j=0 ; j<lung; j++){
+            if(i==lung-1 || j==0 || j==i){
+                printf("*  ");
+            }
+            else{printf("   ");}
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
